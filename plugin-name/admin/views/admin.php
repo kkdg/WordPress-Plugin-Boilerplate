56bea578834dd344d35e478c03e5dde2ed7048b7
Kwag De Gi <tech@skyaperture.com>
2014-01-31 09:51:43 +0900

    woo init

--- plugin-name/admin/views/admin.php
+++ plugin-name/admin/views/admin.php
@@ -5,7 +5,7 @@
  * This includes the header, options, and other information that should provide
  * The User Interface to the end user.
  *
- * @package   Plugin_Name
+ * @package   sa-woo-tester
  * @author    Your Name <email@example.com>
  * @license   GPL-2.0+
  * @link      http://example.com
@@ -18,5 +18,7 @@
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 
 	<!-- @TODO: Provide markup for your options page here. -->
+
+	kkk	
 
 </div>
---
+++ plugin-name/sa-woo-tester.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The WordPress Plugin Boilerplate.
+ *
+ * A foundation off of which to build well-documented WordPress plugins that
+ * also follow WordPress Coding Standards and PHP best practices.
+ *
+ * @package   sa-woo-tester
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2014 Your Name or Company Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       SA-Woo-Tester
+ * Plugin URI:        @TODO
+ * Description:       @TODO
+ * Version:           1.0.0
+ * Author:            @TODO
+ * Author URI:        @TODO
+ * Text Domain:       sa-woo-tester-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/<owner>/<repo>
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// If woocommerce is not activated, cancel plugin activation.
+if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    return;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ * @TODO:
+ *
+ * - replace `class-sa-woo-tester.php` with the name of the plugin's class file
+ *
+ */
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-sa-woo-tester.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ *
+ * @TODO:
+ *
+ * - replace sa-woo-tester with the name of the class defined in
+ *   `class-sa-woo-tester.php`
+ */
+register_activation_hook( __FILE__, array( 'sa_woo_tester', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'sa_woo_tester', 'deactivate' ) );
+
+/*
+ * @TODO:
+ *
+ * - replace sa_woo_tester with the name of the class defined in
+ *   `class-sa_woo_tester.php`
+ */
+add_action( 'plugins_loaded', array( 'sa_woo_tester', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ * @TODO:
+ *
+ * - replace `class-plugin-admin.php` with the name of the plugin's admin file
+ * - replace sa-woo-tester_Admin with the name of the class defined in
+ *   `class-sa-woo-tester-admin.php`
+ *
+ * If you want to include Ajax within the dashboard, change the following
+ * conditional to:
+ *
+ * if ( is_admin() ) {
+ *   ...
+ * }
+ *
+ * The code below is intended to to give the lightest footprint possible.
+ */
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-sa-woo-tester-admin.php' );
+	add_action( 'plugins_loaded', array( 'sa_woo_tester_Admin', 'get_instance' ) );
+
+}
